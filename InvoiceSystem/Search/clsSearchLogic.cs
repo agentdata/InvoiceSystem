624a911697eefd3ddda4bd75c013d7d9d4154005
juansantos192 <46638703+juansantos192@users.noreply.github.com>
2019-04-03 17:15:00 -0600

    Added Juan's files into the Search Folder
    What I have completed so far for the search window

--- InvoiceSystem/Search/clsSearchLogic.cs
+++ InvoiceSystem/Search/clsSearchLogic.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InvoiceSystem.Search
+namespace wndSearch
 {
     class clsSearchLogic
     {
+        //contains the business logic
+        public void SearchInvoiceNum()
+        {
+
+        }
     }
 }
