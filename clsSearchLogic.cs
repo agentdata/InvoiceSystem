11b6a0778665c3e774705dcfb9c8027f4c189503
juansantos192 <46638703+juansantos192@users.noreply.github.com>
2019-04-03 17:10:47 -0600

    Add files via upload
    Adding in Juan's files into the project

---
+++ clsSearchLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wndSearch
+{
+    class clsSearchLogic
+    {
+        //contains the business logic
+        public void SearchInvoiceNum()
+        {
+
+        }
+    }
+}
