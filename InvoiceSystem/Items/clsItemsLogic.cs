2b6e1028f57bcccce5f80c5d9197b47759b568b0
walkerstagg <48969803+walkerstagg@users.noreply.github.com>
2019-04-06 17:05:54 -0600

    Add files via upload

--- InvoiceSystem/Items/clsItemsLogic.cs
+++ InvoiceSystem/Items/clsItemsLogic.cs
@@ -8,16 +8,26 @@ namespace InvoiceSystem.Items
 {
     class clsItemsLogic
     {
+        clsItemsSQL SQL;
+
+        public clsItemsLogic ()
+        {
+            SQL = new clsItemsSQL();
+        }
     }
 
     //Dataset for grid
 
 
     //get all items
+    SQL.GetAllItems();
 
     //add item
+    SQL.AddNewItem();
 
     //save item
+    SQL.UpdateItem();
 
     //delete item
+    SQL.DeleteItem();
 }
--- InvoiceSystem/Items/clsItemsSQL.cs
+++ InvoiceSystem/Items/clsItemsSQL.cs
@@ -8,14 +8,36 @@ namespace InvoiceSystem.Items
 {
     class clsItemsSQL
     {
+
+        //get all items in dataset
+
+
+        public void GetAllItems()
+        {
+            string sSQL = "SELECT * FROM ItemDesc";
+        }
+
+        //add item
+        public void AddNewItem(string itemcode, string itemdesc, string cost)
+        {
+            string sSQL = "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) VALUES (" + itemcode + ", " + 
+                itemdesc + ", " + cost + ")";
+        }
+
+        //save item
+
+        public void UpdateItem(string itemcode, string itemdesc, string cost)
+        {
+            string sSQL = "Update ItemDesc Set ItemCode = " + itemcode + ", ItemDesc = " + itemdesc + ", Cost = " + cost;
+        }
+
+        //delete item
+        public void DeleteItem(string itemcode, string itemdesc, string cost)
+        {
+            string sSQL = "DELTE FROM ItemDesc WHERE ItemCode = " + itemcode + " AND ItemDesc = " + itemdesc + " AND Cost = " + cost;
+        }
     }
 
-    //get all items in dataset
+   
 
-
-    //add item
-
-    //save item
-
-    //delete item
 }
