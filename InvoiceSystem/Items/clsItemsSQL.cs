f59aee4a001e55e9da31983c3cf7a98d17861821
walkerstagg <48969803+walkerstagg@users.noreply.github.com>
2019-04-20 11:58:26 -0600

    Add files via upload

--- InvoiceSystem/Items/clsItemsSQL.cs
+++ InvoiceSystem/Items/clsItemsSQL.cs
@@ -1,41 +1,131 @@
-﻿using System;
+﻿using InvoiceSystem.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+using System.Data.SqlClient;
 
 namespace InvoiceSystem.Items
 {
     class clsItemsSQL
     {
-
-        //get all items in dataset
-
-
-        public void GetAllItems()
+        /// <summary>
+        /// Instance of the data Access Class.
+        /// </summary>
+        clsDataAccess Data;
+        
+        /// <summary>
+        /// clsItemsSQL Constructor.  Instanitiates data access class.
+        /// </summary>
+        public clsItemsSQL ()
         {
-            string sSQL = "SELECT * FROM ItemDesc";
+            Data = new clsDataAccess();
         }
 
-        //add item
+        #region Queries
+        /// <summary>
+        /// Retrieves all items in the database and returns an Item List.
+        /// </summary>
+        /// <returns></returns>
+        public List<Item> GetAllItems()
+        {
+            try
+            {
+                List<Item> output = new List<Item>();
+
+                string sSQL = "SELECT * FROM ItemDesc";
+                int iRetVal = 0;
+
+                DataSet ds = Data.ExecuteSQLStatement(sSQL, ref iRetVal);
+
+                foreach  (DataRow dr in ds.Tables[0].Rows)
+                {
+                    output.Add(new Item(dr[0].ToString(), dr[1].ToString(), dr[2].ToString()));
+                }
+
+                return output;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region NonQueries
+        /// <summary>
+        /// Adds an item to the database.
+        /// </summary>
+        /// <param name="itemcode"></param>
+        /// <param name="itemdesc"></param>
+        /// <param name="cost"></param>
         public void AddNewItem(string itemcode, string itemdesc, string cost)
         {
-            string sSQL = "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) VALUES (" + itemcode + ", " + 
-                itemdesc + ", " + cost + ")";
+            try
+            {
+                string sSQL = "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) VALUES ('" + itemcode + "', '" +
+                itemdesc + "', '" + cost + "')";
+
+                Data.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
 
-        //save item
-
+        /// <summary>
+        /// Updates the corresponding Item in the Database.  Sets the Cost and Item Description
+        /// based on a matching Item code.
+        /// </summary>
+        /// <param name="itemcode"></param>
+        /// <param name="itemdesc"></param>
+        /// <param name="cost"></param>
         public void UpdateItem(string itemcode, string itemdesc, string cost)
         {
-            string sSQL = "Update ItemDesc Set ItemCode = " + itemcode + ", ItemDesc = " + itemdesc + ", Cost = " + cost;
+            try
+            {
+                string sSQL = "Update ItemDesc Set ItemDesc = '" + itemdesc + "', Cost = '" + cost + "' WHERE ItemCode = '" + itemcode + "'";
+
+                Data.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
         }
 
-        //delete item
+        /// <summary>
+        /// Deletes the corresponding Item in the database based on Item Code.
+        /// </summary>
+        /// <param name="itemcode"></param>
+        /// <param name="itemdesc"></param>
+        /// <param name="cost"></param>
         public void DeleteItem(string itemcode, string itemdesc, string cost)
         {
-            string sSQL = "DELTE FROM ItemDesc WHERE ItemCode = " + itemcode + " AND ItemDesc = " + itemdesc + " AND Cost = " + cost;
+            try
+            {
+                string sSQL = "DELETE FROM ItemDesc WHERE ItemCode = '" + itemcode + "'"; 
+
+                Data.ExecuteNonQuery(sSQL);
+
+                clsItemsLogic.ItemsList.ResetBindings();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
+        #endregion
     }
 
    
--- InvoiceSystem/Items/wndItems.xaml.cs
+++ InvoiceSystem/Items/wndItems.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using InvoiceSystem.Main;
 using InvoiceSystem.OtherClasses;
+using InvoiceSystem.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,61 +24,381 @@ namespace InvoiceSystem.Items
     /// </summary>
     public partial class wndItems : UserControl
     {
-        //Fields
+        #region Variables
+        /// <summary>
+        /// Instance of class that navigates through the different user Controls.
+        /// </summary>
         private readonly ViewNavigationController viewNavigationController;
 
         /// <summary>
-        /// Flag to determine if the window is in editing mode.
+        /// Item Object to hold selected Items and it's properties.
+        /// </summary>
+        private Item item;
+
+        /// <summary>
+        /// Boolean setting for noting when the Edit button has been pressed and
+        /// the user is editing an item.
         /// </summary>
         private bool IsEditing;
+        #endregion
 
+        #region HelperMethods
+        /// <summary>
+        /// Contructor for the wndItems window.
+        /// </summary>
+        /// <param name="viewNavigationController"></param>
         public wndItems(ViewNavigationController viewNavigationController)
         {
-            InitializeComponent();
-            this.viewNavigationController = viewNavigationController;
-            IsEditing = false;
+            try
+            {
+                InitializeComponent();
+                this.viewNavigationController = viewNavigationController;
+                item = new Item("", "", "");
+                
+                BeginningProperties();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
         }
 
+        /// <summary>
+        /// Sets the properites of window objects for initial use.
+        /// </summary>
         private void BeginningProperties()
         {
-            //disable CodeText, CostText, and DescText -- Possible just read-only
-            //disable CancelButton & SubmitButton
+            try
+            {
+                clsItemsLogic.GetItems();
+                ItemDataGrid.ItemsSource = clsItemsLogic.ItemsList;
 
+                //disable CodeText, CostText, and DescText
+                CostText.IsReadOnly = true;
+                DescText.IsReadOnly = true;
+
+                //disable CancelButton & SubmitButton
+                CancelButton.IsEnabled = false;
+                SubmitButton.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Returns window objects to a proper state after a user completes
+        /// an add, edit, or delete.
+        /// </summary>
+        private void ReadView()
+        {
+            try
+            {
+                CodeText.IsEnabled = false;
+
+                AddButton.IsEnabled = true;
+                DeleteButton.IsEnabled = true;
+
+                //Text Boxes non-editable
+                CostText.IsReadOnly = true;
+                DescText.IsReadOnly = true;
+
+                //Submit and Cancel Disabled
+                SubmitButton.IsEnabled = false;
+                CancelButton.IsEnabled = false;
+                EditButton.IsEnabled = true;
+                AddButton.IsEnabled = true;
+                DeleteButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+
+        }
+        #endregion
+
+        #region Selection/Validation
+        /// <summary>
+        /// Event that is triggered when a selection is made in the Item Data Grid.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ItemSelection(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+
+                if (!IsEditing)
+                {
+                    int index = ItemDataGrid.SelectedIndex;
+
+                    CodeText.Text = clsItemsLogic.ItemsList[index].ItemCode;
+                    CostText.Text = clsItemsLogic.ItemsList[index].Cost;
+                    DescText.Text = clsItemsLogic.ItemsList[index].ItemDesc;
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Error Handling method for Friendly Error output.
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (System.Exception ex)
+            {
+                System.IO.File.AppendAllText(@"C:\Error.txt", Environment.NewLine + "HandleError Exception: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Allows the user to only use numbers and a period, as well as
+        /// backspace, delete, tab, and enter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NumberValidation(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                //Number
+                if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
+                {
+                    //KeyPad
+                    if (!(e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9))
+                    {
+                        //Allow backspace, delete, tab and enter
+                        if (!(e.Key == Key.Back || e.Key == Key.Delete || e.Key == Key.Tab || e.Key == Key.Enter || e.Key == Key.Decimal))
+                        {
+                            e.Handled = true;
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// allows the user to use alpha numeric keys along with backspace,
+        /// delete, tab, and enter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void KeyValidation(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                //Letters & numbers
+                if (!(e.Key >= Key.D0 && e.Key <= Key.Z))
+                {
+                    //Allow backspace, delete, tab and enter
+                    if (!(e.Key == Key.Back || e.Key == Key.Delete || e.Key == Key.Tab || e.Key == Key.Enter || e.Key == Key.Space))
+                    {
+                        e.Handled = true;
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Buttons
+        /// <summary>
+        /// Action for the Add Button.  Disables buttons and 
+        /// changes the cost, code, and description text boxes
+        /// to not be read-only.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            //Changes grid select to be -1 (or similar)
-            //Clears CodeText, CostText, and DescText
-            //IsEditing = true;
-            //enable Submit button;
+            try
+            {
+                //Changes grid select to be -1 (or similar)
+                //Clears CodeText, CostText, and DescText
+                //enable Submit/Cancel button;
+                
+                CodeText.Text = "";
+                DescText.Text = "";
+                CostText.Text = "";
+
+                CodeText.IsEnabled = true;
+                CostText.IsReadOnly = false;
+                DescText.IsReadOnly = false;
+
+                SubmitButton.IsEnabled = true;
+                CancelButton.IsEnabled = true;
+                AddButton.IsEnabled = false;
+                EditButton.IsEnabled = false;
+                DeleteButton.IsEnabled = false;
+                
+                
+
+
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Action for the Edit Button.  Disables, buttons and changes cost and description
+        /// text boxes from read-only.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
-            //isEditing = true;
-            //disable Addbutton and DeleteButton
-            //Enable CodeText, CostText, and DescText
+            
+            try
+            {
+                IsEditing = true;
+
+                AddButton.IsEnabled = false;
+                DeleteButton.IsEnabled = false;
+                EditButton.IsEnabled = false;
+
+                //Text Boxes editable
+                CostText.IsReadOnly = false;
+                DescText.IsReadOnly = false;
+
+                //Submit and Cancel Enabled
+                SubmitButton.IsEnabled = true;
+                CancelButton.IsEnabled = true;
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Action for the Delete Button.  Removes item from the Data Grid and Database.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            //calls SQL statement to delete current selected item
+            
+            try
+            {
+                int index = ItemDataGrid.SelectedIndex;
+                string code = CodeText.Text;
+                string desc = DescText.Text;
+                string cost = CostText.Text;
+
+                if (index > 0)
+                {
+                    ItemDataGrid.SelectedIndex = index -1;
+                }
+                else
+                {
+                    ItemDataGrid.SelectedIndex = -1;
+                }
+
+
+                clsItemsLogic.DeleteItem(code, desc, cost, index);
+                CodeText.Text = "";
+                CostText.Text = "";
+                DescText.Text = "";
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Action for the Cancel Button.  Returns window to the standard view.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            //can only be used if isEditing is true
-            //end iwht isEditing = false;
-            // disable CodeText, CostText, and DescText and CANCEL/SUBMIT buttons
+            
+            try
+            {
+                item = (Item)ItemDataGrid.SelectedItem;
+
+                CodeText.Text = item.ItemCode;
+                CostText.Text = item.Cost;
+                DescText.Text = item.ItemDesc;
+
+                ReadView();
+
+                IsEditing = false;
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Action for the Submit Button.  If the user is editing, it updates the Data grid
+        /// and database.  If not, it adds to the list and the database.  Then returns to 
+        /// a normal view.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
-            //can only be used if isEditing is true
-            //SQL update for current selected Item
-            //end iwht isEditing = false;
-            // disable CodeText, CostText, and DescText and CANCEL/SUBMIT buttons
+            
+            try
+            {
+                if (IsEditing)
+                {
+                    clsItemsLogic.UpdateItem(CodeText.Text, DescText.Text, CostText.Text, ItemDataGrid.SelectedIndex);
+                    ReadView();
+                    IsEditing = false;
+                }
+                else
+                {
+                    clsItemsLogic.AddItem(CodeText.Text, DescText.Text, CostText.Text);
+                    ReadView();
+                    ItemDataGrid.SelectedIndex = clsItemsLogic.ItemsList.Count - 1;
+                }
+
+                
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
         /// <summary>
@@ -86,7 +408,19 @@ namespace InvoiceSystem.Items
         /// <param name="e"></param>
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-            viewNavigationController.ChangeCurrentView(new wndMain(viewNavigationController));
+            try
+            {
+                ReadView();
+                viewNavigationController.ChangeCurrentView(new wndMain(viewNavigationController));
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
+        #endregion
+
+        
     }
 }
