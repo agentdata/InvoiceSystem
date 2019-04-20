ce8152ae134734d749de802050e2c01f3c61421b
walkerstagg <48969803+walkerstagg@users.noreply.github.com>
2019-04-20 12:00:13 -0600

    Add files via upload
    Added XML comments for the Item Class.

--- InvoiceSystem/Classes/Item.cs
+++ InvoiceSystem/Classes/Item.cs
@@ -8,10 +8,25 @@ namespace InvoiceSystem.Classes
 {
     public class Item
     {
+        /// <summary>
+        /// Corresponds to the ItemCode Attribute in the ItemDesc Table.
+        /// </summary>
         public string ItemCode { get; set; }
+        /// <summary>
+        /// Corresponds to the ItemDesc Attribute in the ItemDesc Table.
+        /// </summary>
         public string ItemDesc { get; set; }
+        /// <summary>
+        /// Corresponds ot the Cost Attribute in the ItemDesc Table.
+        /// </summary>
         public string Cost { get; set; }
 
+        /// <summary>
+        /// Item Class constructor.  Sets all public properties.
+        /// </summary>
+        /// <param name="ItemCode"></param>
+        /// <param name="ItemDesc"></param>
+        /// <param name="Cost"></param>
         public Item(string ItemCode, string ItemDesc, string Cost)
         {
             this.ItemCode = ItemCode;
