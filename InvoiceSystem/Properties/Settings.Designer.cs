fd5b1b13c082c566fb5548efd87c22cfb743bb8d
brantly <brantly.w@gmail.com>
2019-04-21 21:43:20 -0600

    removing traces of invoiceDB from properties and settings.

--- InvoiceSystem/Properties/Settings.Designer.cs
+++ InvoiceSystem/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace InvoiceSystem.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,16 +20,6 @@ namespace InvoiceSystem.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\InvoiceDB.accdb")]
-        public string InvoiceDBConnectionString {
-            get {
-                return ((string)(this["InvoiceDBConnectionString"]));
             }
         }
     }
