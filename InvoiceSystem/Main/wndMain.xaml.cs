141baa80c04adb350dd303ac90f7ef04f40a953c
agentdata <brantly.w@gmail.com>
2019-04-21 23:34:37 -0600

    Revert "more comments, I don't think there are any functional changes in this one."
    This reverts commit 2c9ba882a28788e734edbede15c8b01553a57606.

--- InvoiceSystem/Main/wndMain.xaml.cs
+++ InvoiceSystem/Main/wndMain.xaml.cs
@@ -48,7 +48,7 @@ namespace InvoiceSystem.Main
             Invoices = clsMainLogic.getAllInvoices(Invoices);
             DataContext = this;
         }
-        #endregion Constructors
+        #endregion
 
         #region UI Action
         /// <summary>
--- InvoiceSystem/MainWindow.xaml.cs
+++ InvoiceSystem/MainWindow.xaml.cs
@@ -26,9 +26,6 @@ namespace InvoiceSystem
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        /// <summary>
-        /// This constructor is only used on first run of the application.
-        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -38,9 +35,7 @@ namespace InvoiceSystem
         }
 
         //Properties
-        //this tracks which view should be displayed.
         private UserControl mainView;
-        //UI contentpresenter is bound to mainview
         public UserControl MainView
         {
             get
@@ -58,88 +53,47 @@ namespace InvoiceSystem
         }
 
         //Fields
-        //the navigation controller which controls the view shown in the window
         private readonly ViewNavigationController viewNavigationController = new ViewNavigationController();
 
         //Events
         public event PropertyChangedEventHandler PropertyChanged;
 
-        /// <summary>
-        /// When the viewcontroller changes the mainview is updated, the UI contentpresenter is bound to this mainview object
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="userControl"></param>
         private void ViewNavigationController_CurrentViewChanged(object sender, UserControl userControl)
         {
             MainView = userControl;
         }
 
         #region Menu Buttons
-        /// <summary>
-        /// Settings button action, opens the settings view
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void MenuBarSettingsButton_Action(object sender, RoutedEventArgs e)
         {
             viewNavigationController.ChangeCurrentView(new Settings(viewNavigationController));
         }
 
-        /// <summary>
-        /// exit button action, exits the application
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void MenuBarExitButton_Action(object sender, RoutedEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
         }
 
-        /// <summary>
-        /// Add New Invoice button action, opens the new Invoice view
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void AddNewInvoiceMenuButton_Action(object sender, RoutedEventArgs e)
         {
             viewNavigationController.ChangeCurrentView(new NewInvoice(viewNavigationController));
         }
 
-        /// <summary>
-        /// search invoices button action, opens the search view
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void SearchInvoicesMenuButton_Action(object sender, RoutedEventArgs e)
         {
             viewNavigationController.ChangeCurrentView(new wndSearch(viewNavigationController));
         }
 
-        /// <summary>
-        /// edit items button action, opens the edit items view
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void EditItemsMenuButton_Action(object sender, RoutedEventArgs e)
         {
             viewNavigationController.ChangeCurrentView(new wndItems(viewNavigationController));
         }
 
-        /// <summary>
-        /// main button action, opens the main dashboard view
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void MenuBarMainButton_Action(object sender, RoutedEventArgs e)
         {
             viewNavigationController.ChangeCurrentView(new wndMain(viewNavigationController));
         }
 
-        /// <summary>
-        /// about button action, opens the about window
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void MenuBarAboutItem_Action(object sender, RoutedEventArgs e)
         {
             MainContentPresenter_ContentPresenter.IsEnabled = false;
@@ -153,16 +107,13 @@ namespace InvoiceSystem
         #endregion Menu Buttons
 
         #region UI 
-        /// <summary>
-        /// updates the blur feature when the menu item is visible
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+
         private void ChangeDisabledForegroundAndOpacity_Action(object sender, DependencyPropertyChangedEventArgs e)
         {
             if (MainContentPresenter_ContentPresenter.IsEnabled)
             {
                 OpaqueContentCover_Label.Visibility = Visibility.Hidden;
+
             }
             else
             {
@@ -170,11 +121,6 @@ namespace InvoiceSystem
             }
         }
 
-        /// <summary>
-        /// updates the blur feature when the menu item is visible
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void MenuItem_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if ((bool)e.NewValue)
